70800c4db9444a4ce5270017776d5d5858f1ac5b
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-08-22 00:11:48 +0200

    creado el hook

--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const FormComponent = () => {
+
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formState;
+
+  const handleForm = ({ target }) => {
+    const { value, name } = target
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  }
+
+  const onsubmitForm = (event) => {
+    event.preventDefault()
+    console.log(formState)
+  }
+
   return (
-    <form>
+    <form onSubmit={onsubmitForm}>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email address</label>
         <input
           type="email"
           className="form-control"
           name="email"
+          value={email}
+          onChange={handleForm}
         />
       </div>
       <div className="mb-3">
@@ -17,6 +40,8 @@ export const FormComponent = () => {
           type="password"
           className="form-control"
           name="password"
+          value={password}
+          onChange={handleForm}
         />
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
